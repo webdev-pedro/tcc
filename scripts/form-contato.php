e32a835202e80a3031ca5ae819f13b1e5f364c04
Pedro Melo <phenriqmelo99@gmail.com>
2019-05-07 20:51:03 -0300

    remodelando o site

---
+++ scripts/form-contato.php
@@ -0,0 +1,36 @@
+﻿<?php 
+
+// Dados do Formulário
+
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$telefone = $_POST["tel"];
+$assunto = $_POST["assunto"];
+$mensagem = $_POST["mensagem"];
+    
+// Configurações de email
+
+$email_destinatario = "	contato@zuresolutions.dx.am";
+$email_reply = "$email"; 
+$email_assunto = "Zure Solutions - Contato";
+	
+// Estrutura de mensagem
+	
+$email_conteudo = "Zure Solutions - Contato: \n \n"; 
+$email_conteudo .= "Nome: $nome \n"; 
+$email_conteudo .= "Email: $email \n";
+$email_conteudo .= "Telefone: $telefone \n";
+$email_conteudo .= "Assunto: $assunto \n";
+$email_conteudo .= "Mensagem: $mensagem \n"; 
+
+// Seta os Headers
+
+$headers = "MIME-Version: 1.1\r\n";
+$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+$headers .= "From: phenriqmelo99@gmail.com\r\n"; // remetente
+$headers .= "Return-Path: contato@zuresolutions.dx.am\r\n"; // return-path
+
+mail($email_destinatario, $email_assunto, $email_conteudo, $headers, "-r".$email_destinatario);
+
+?>
+	
---
+++ scripts/index.js
@@ -0,0 +1,200 @@
+$(document).ready(function(){
+	
+/* ---------- Form contato ---------- */	
+	
+	$("#btn-enviar").click(function(e){
+
+        var dados = $("#r-form").serialize();
+        
+		if($("#nome").val() == "")
+		{
+			$("#erro-nome").fadeIn(300);
+			$("#erro-nome").delay(2000).fadeOut(1000);
+		}
+		
+		else
+		{
+			$("#erro-nome").fadeOut(300);
+			
+			if($("#email").val() == "")
+			{
+				$("#erro-email").delay(300).fadeIn(300);
+				$("#erro-email").delay(2000).fadeOut(1000);
+				
+			}
+			
+			else
+			{
+				$("#erro-email").fadeOut(300);
+					
+				if($("#tel").val() == "")
+				{
+					$("#erro-telefone").delay(300).fadeIn(300);
+					$("#erro-telefone").delay(2000).fadeOut(1000);
+				}	
+				else
+				{
+					$("#erro-telefone").fadeOut(300);
+						
+					if($("#assunto").val() == ""){
+						$("#erro-assunto").delay(300).fadeIn(300);
+						$("#erro-assunto").delay(2000).fadeOut(1000);
+					}	
+				
+				else
+				{
+					$("#erro-telefone").fadeOut(300);
+					
+					if($("#mensagem").val() == "")
+					{
+						$("#erro-mensagem").delay(300).fadeIn(300);
+						$("#erro-mensagem").delay(2000).fadeOut(1000);
+					}
+
+					else
+					{
+						$("#erro-mensagem").fadeOut(300)
+						$("#success").delay(300).fadeIn(300);
+						$("#success").delay(3000).fadeOut(1000);
+
+						$.ajax({
+							type: "POST", 
+							url: "scripts/form.php", 
+							dataType:"html", 
+							data:dados, 
+							success:function(e){                             
+								$("#nome").val("");
+								$("#email").val("");
+								$("#tel").val("");
+								$("#assunto").val("");
+								$("#mensagem").val("");
+							}
+						});
+
+					}
+					
+				}
+				
+			}
+		}
+	}
+		
+});	
+	
+/* ------------------------------------------------------------ Responsive --------------------------------------------------------- */
+/* ---------- Contato ---------- */	
+	
+	$("#r-btn-enviar").click(function(e){
+
+        var dados = $("#r-form").serialize();
+        
+		if($("#nome").val() == "")
+		{
+			$("#erro-nome").fadeIn(300);
+			$("#erro-nome").delay(2000).fadeOut(1000);
+		}
+		
+		else
+		{
+			$("#erro-nome").fadeOut(300);
+			
+			if($("#email").val() == "")
+			{
+				$("#erro-email").delay(300).fadeIn(300);
+				$("#erro-email").delay(2000).fadeOut(1000);
+			}
+			
+			else
+			{
+				$("#erro-email").fadeOut(300);
+					
+				if($("#tel").val() == ""){
+					$("#erro-telefone").delay(300).fadeIn(300);
+					$("#erro-telefone").delay(2000).fadeOut(1000);
+				}	
+
+				else
+				{
+					$("#erro-telefone").fadeOut(300);
+						
+					if($("#assunto").val() == ""){
+						$("#erro-assunto").delay(300).fadeIn(300);
+						$("#erro-assunto").delay(2000).fadeOut(1000);
+					}	
+					
+				else
+				{	
+					$("#erro-telefone").fadeOut(300);	
+					
+					if($("#mensagem").val() == "")
+					{
+						$("#erro-mensagem").delay(300).fadeIn(300);
+						$("#erro-mensagem").delay(2000).fadeOut(1000);
+					}
+
+					else
+					{
+						$("#erro-mensagem").fadeOut(300)
+						
+						$("#success").delay(300).fadeIn(300);
+						$("#success").delay(3000).fadeOut(1000);
+
+						$.ajax({
+
+							type: "POST", 
+							url: "scripts/form.php", 
+							dataType:"html", 
+							data:dados, 
+							success:function(e){                              
+								$("#nome").val("");
+								$("#email").val("");
+								$("#tel").val("");
+								$("#assunto").val("");
+								$("#mensagem").val("");
+							}
+
+						});
+						}
+					}			
+				}	
+			}
+		}
+	});	
+
+	ScrollReveal().reveal('.titulo', { delay: 500, duration: 1000 });
+	ScrollReveal().reveal('.texto', { delay: 750, duration: 1000});
+
+	$("nav a").click(function(e){			
+		event.preventDefault();		
+		$("html, body").animate({scrollTop: $($(this).attr('href')).offset().top - 50}, 800);		
+	});
+
+	$(".down").click(function(e){			
+		event.preventDefault();		
+		$("html, body").animate({scrollTop: $($(this).attr('href')).offset().top - 50}, 800);		
+	});
+
+	$("nav li").click(function(e){		
+		$("nav li").removeClass("ativo");		
+		$(this).addClass("ativo");				
+	});
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop() >= 550) {
+			$('nav').addClass('header-fixo');
+		}
+		else {
+			$('nav').removeClass('header-fixo');
+		}
+	});
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop() >= 600) {
+			$('.botao-subir').css('display', 'block');
+		}
+		else {
+			$('.botao-subir').css('display', 'none');
+		}
+	});
+});
+
